1404d947341fd1d078d15fd95ce2d64c53026a46
Mateus Thives <mateus.thives@hotmail.com>
2026-03-25 21:47:53 -0300

    Todas as alterações concluidas

--- jogoDeAdivinhacao.ConsoleApp/Program.cs
+++ jogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -20,6 +20,20 @@ Fácil (intervalo 1 a 20): ≈ 10 tentativas.
 Médio (intervalo 1 a 50): ≈ 5 tentativas.
 Difícil (intervalo 1 a 100): ≈ 3 tentativas.
 
+2. Implemente uma funcionalidade de Validação de Números Repetidos
+
+O jogador deve ser informado caso o número que está tentando adivinhar já tenha sido informado anteriormente na mesma rodada.
+
+3. Implemente uma funcionalidade de Pontuação, onde:
+
+O jogador começa com uma pontuação máxima, por exemplo, 1000 pontos.
+A pontuação é calculada com base na proximidade do palpite em relação ao número secreto.
+A cada tentativa errada, o jogador perde pontos de acordo com a distância do número secreto:
+Se a diferença entre o número secreto e o palpite for de 10 ou mais, o jogador perde 100 pontos.
+Se a diferença for entre 5 e 9, o jogador perde 50 pontos.
+Se a diferença for entre 1 e 4, o jogador perde 20 pontos.
+Quando o jogador acerta o número, sua pontuação final é registrada.
+
 */
 
 bool jogoDeveContinuar = true;
@@ -34,12 +48,13 @@ while (jogoDeveContinuar == true)
     Console.WriteLine("-----------------------------------");
     Console.WriteLine("1 - Fácil (10 tentativas)");
     Console.WriteLine("2 - Médio (5 tentativas)");
-    Console.WriteLine("1 - Difícil (3 tint numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);entativas)");
+    Console.WriteLine("1 - Difícil (3 tentativas)");
     Console.WriteLine("-----------------------------------");
 
     Console.Write("Digite sua escolha: ");
     string dificuldadeEscolhida = Console.ReadLine();
 
+    int totalDeTentativas;
     int numeroAleatorio;
     int tentativasMaximas;
 
@@ -69,6 +84,11 @@ while (jogoDeveContinuar == true)
 
     }
 
+    int pontuacao = 1000;
+
+    int[] numerosDigitados = new int[tentativasMaximas];
+    int contadorNumerosDigitados = 0;
+
     // enquanto a tentativa atual for menor que a quantidade de tentativas máximas
     for (int tentativaAtual = 1; tentativaAtual <= tentativasMaximas; tentativaAtual++)
     {
@@ -78,8 +98,49 @@ while (jogoDeveContinuar == true)
         Console.WriteLine("-----------------------------------");
         Console.WriteLine($"Tentativa {tentativaAtual} de {tentativasMaximas}");
         Console.WriteLine("-----------------------------------");
+
         Console.WriteLine("Digite um número: ");
-        int numeroDigitado = Convert.ToInt32(Console.ReadLine())
+        int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+        // comparar com outros números já na memória
+        bool numeroEstaRepetido = false;
+
+        for (int indiceAtual = 0; indiceAtual < numerosDigitados.Length; indiceAtual++)
+        {
+            if (numerosDigitados[indiceAtual] == numeroDigitado)
+            {
+                numeroEstaRepetido = true;
+                break;
+            }
+        }
+
+        if (numeroEstaRepetido == true)
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Você já digitou esse número");
+            Console.WriteLine("-----------------------------------");
+
+            Console.WriteLine("Digite ENTER para continuar...");
+            Console.ReadLine();
+
+            tentativaAtual--;
+            continue;
+        }
+
+        // guardar numero na memória
+        if (contadorNumerosDigitados < numerosDigitados.Length)
+        {
+            numerosDigitados[contadorNumerosDigitados] = numeroDigitado;
+            contadorNumerosDigitados++;
+        }
+        else
+        {
+            numerosDigitados = new int[tentativasMaximas];
+            contadorNumerosDigitados = 0;
+
+            numerosDigitados[contadorNumerosDigitados] = numeroDigitado;
+            contadorNumerosDigitados++;
+        }
 
         if (numeroDigitado == numeroAleatorio)
         {
@@ -91,18 +152,41 @@ while (jogoDeveContinuar == true)
         {
             Console.WriteLine("O número digitado foi maior que o número secreto!");
         }
-
         else
         {
             Console.WriteLine("O número digitado foi menor que o número secreto!");
         }
 
+        int diferencaNumerica = Math.Abs(numeroAleatorio - numeroDigitado); // 90 - 100 = -10
+
+        /*
+
+        Se a diferença entre o número secreto e o palpite for de 10 ou mais, o jogador perde 100 pontos.
+        Se a diferença for entre 5 e 9, o jogador perde 50 pontos.
+        Se a diferença for entre 1 e 4, o jogador perde 20 pontos.
+
+        */
+
+        if (diferencaNumerica >= 10)
+        {
+            pontuacao -= 100;
+        }
+        else if (diferencaNumerica >= 5)
+        {
+            pontuacao -= 50;
+        }
+        else
+        {
+            pontuacao -= 20;
+        }
+
         Console.WriteLine("-----------------------------------");
-        Console.WriteLine("Digite ENTER para continuar...")
+        Console.WriteLine("Sua pontuação é: " + pontuacao);
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("Digite ENTER para continuar...");
         Console.ReadLine();
 
     }
-
 
     Console.WriteLine();
     Console.Write("Deseja continuar? (s/N): ");
